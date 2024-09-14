6f69b3c2928432c7c2a0358994ab1b582265608f
Lauro Gripa <laurogripa@gmail.com>
2024-09-14 17:47:06 -0300

    Fix eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,25 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
 import pluginReact from "eslint-plugin-react";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  eslintPluginPrettierRecommended,
   {
-    settings: {
-      react: {
-        version: "detect",
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: parser,
+      globals: {
+        ...globals.browser,
+        process: "readonly",
+        React: "writable",
+        require: "readonly",
       },
+    },
+    plugins: {
+      react: pluginReact,
+      "@typescript-eslint": tseslint,
     },
     rules: {
       "max-len": ["warn", { code: 120 }],
@@ -29,6 +33,21 @@ export default [
       ],
     },
   },
+  {
+    ignores: [
+      ".pnp.cjs",
+      "node_modules/**",
+      "dist/**",
+      "build/**",
+      ".next/**",
+      ".github/**",
+      ".storybook/**",
+      ".yarn/**",
+      "**/vendor/**",
+    ],
+  },
+  pluginJs.configs.recommended,
+  eslintPluginPrettierRecommended,
   {
     files: ["prettier.config.js"],
     languageOptions: {
