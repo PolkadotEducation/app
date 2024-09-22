77d33c97efe592f721fe053a7e3a5c4ca8eebb4c
Lauro Gripa <laurogripa@gmail.com>
2024-09-22 15:58:50 -0300

    Create seed with admin user

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "cypress";
-import { dropDatabase } from "./cypress/support/db";
+import { setupDatabase } from "./cypress/support/db";
 
 export default defineConfig({
   e2e: {
@@ -8,11 +8,8 @@ export default defineConfig({
     supportFile: "cypress/support/index.ts",
     supportFolder: "cypress/support",
     setupNodeEvents(on, config) {
-      on("task", {
-        async dropDatabase() {
-          await dropDatabase();
-          return null;
-        },
+      on("before:run", async () => {
+        await setupDatabase();
       });
 
       return config;
--- cypress/support/db.ts
+++ cypress/support/db.ts
@@ -1,21 +1,56 @@
 /* eslint-disable no-console */
-import { MongoClient } from "mongodb";
+import { Db, MongoClient } from "mongodb";
 
-async function dropDatabase() {
-  const uri = "mongodb://localhost:27117";
+const uri = "mongodb://localhost:27117";
+const dbName = "doteducation";
+
+async function connectToDatabase() {
   const client = new MongoClient(uri);
+  console.info("Connecting to MongoDB at:", uri);
+  await client.connect();
+  const db = client.db(dbName);
+  return { client, db };
+}
 
+async function dropDatabase(db: Db) {
   try {
-    console.info("Connecting to MongoDB at:", uri);
-    await client.connect();
-    const db = client.db("doteducation");
     await db.dropDatabase();
     console.info("Database dropped.");
   } catch (error) {
     console.error("Error dropping database:", error);
+  }
+}
+
+async function seedDatabase(db: Db) {
+  try {
+    const adminUser = {
+      email: "admin@seed.com",
+      password: "43e03e54352a95fbe19a7a1b6e1d076ef170f6dc3be5a5eebe8ccc23d4ead11d",
+      name: "Seeded Admin",
+      company: "Polkadot Education",
+      isAdmin: true,
+      verifyToken: "11796407f2d34450d955184cf66544b4",
+      lastActivity: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    await db.collection("users").insertOne(adminUser);
+    console.info("Database seeded.");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  }
+}
+
+async function setupDatabase() {
+  const { client, db } = await connectToDatabase();
+
+  try {
+    await dropDatabase(db);
+    await seedDatabase(db);
   } finally {
     await client.close();
   }
 }
 
-export { dropDatabase };
+export { dropDatabase, seedDatabase, setupDatabase };
