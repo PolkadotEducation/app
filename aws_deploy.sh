490093d6a2d315d896ccd207fa7957983eaea102
Artur Gontijo <arturgontijo@gmail.com>
2024-12-02 18:14:46 -0300

    Add aws_deploy.sh to create .env file before build.

--- aws_deploy.sh
+++ aws_deploy.sh
@@ -6,9 +6,9 @@ echo "NEXT_PUBLIC_AUTH_TOKEN=${NEXT_PUBLIC_AUTH_TOKEN}" >> .env
 echo "GOOGLE_CLIENT_ID=${GOOGLE_CLIENT_ID}" >> .env
 echo "GOOGLE_CLIENT_SECRET=${GOOGLE_CLIENT_SECRET}" >> .env
 
-# curl -fsSL https://bun.sh/install
+curl -fsSL https://bun.sh/install
 
-# export BUN_INSTALL="$HOME/.bun"
-# export PATH="$BUN_INSTALL/bin:$PATH"
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
 bun install
 bun run build
