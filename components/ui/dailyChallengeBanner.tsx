dbbec35a79cf7897c036ef750bd58118995c2709
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2024-12-13 17:52:33 -0300

    fix dialog open and closing logic

--- components/ui/dailyChallengeBanner.tsx
+++ components/ui/dailyChallengeBanner.tsx
@@ -1,41 +1,46 @@
 import React, { useState } from "react";
 import tutorialIllustration from "../../public/assets/images/tutorial-illustration.svg";
 import Image from "next/image";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "./dialog";
+import {
+  DialogHeader,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogOverlay,
+} from "./dialog";
 
 const DailyChallengeBanner = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleOpenDailyChallenge = () => {
-    setIsOpen(!isOpen);
-  };
-
+  console.log("Antes do set:", isOpen);
   return (
-    <div
-      onClick={() => handleOpenDailyChallenge()}
-      className="flex flex-1 rounded-lg bg-secondary-light p-4 md:p-5 items-center my-4 cursor-pointer"
-    >
-      <div className="flex gap-6 flex-col">
-        <h4 className="text-xl md:text-4xl">Desafio do dia</h4>
-        <p>Complete o desafio, ganhe pontos e avance no ranking!</p>
+    <>
+      <div
+        onClick={() => setIsOpen(true)}
+        className="flex flex-1 rounded-lg bg-secondary-light p-4 md:p-5 items-center my-4 cursor-pointer"
+      >
+        <div className="flex gap-6 flex-col">
+          <h4 className="text-xl md:text-4xl">Desafio do dia</h4>
+          <p>Complete o desafio, ganhe pontos e avance no ranking!</p>
+        </div>
+        <Image
+          src={tutorialIllustration}
+          alt="Tutorial image"
+          className="ml-auto w-[160px] h-[160px] md:w-[240px] md:h-[240px]"
+        />
       </div>
-      <Image
-        src={tutorialIllustration}
-        alt="Tutorial image"
-        className="ml-auto w-[160px] h-[160px] md:w-[240px] md:h-[240px]"
-      />
-      <Dialog open={isOpen}>
-        <DialogContent className="pointer-events-none">
+
+      <Dialog open={isOpen} onOpenChange={(open) => setIsOpen(open)}>
+        <DialogContent showCloseButton={true}>
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your account and remove your data from our
-              servers.
-            </DialogDescription>
+            <DialogTitle>Desafio do dia</DialogTitle>
+            <DialogDescription>Enunciado da pergunta</DialogDescription>
           </DialogHeader>
         </DialogContent>
       </Dialog>
-    </div>
+    </>
   );
 };
 
