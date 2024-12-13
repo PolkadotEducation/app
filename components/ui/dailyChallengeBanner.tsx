342571573b8cbad2c40aa5c22c65bbbdbc39c780
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2024-12-12 22:41:58 -0300

    add dialog

--- components/ui/dailyChallengeBanner.tsx
+++ components/ui/dailyChallengeBanner.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import tutorialIllustration from "../../public/assets/images/tutorial-illustration.svg";
 import Image from "next/image";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "./dialog";
 
 const DailyChallengeBanner = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenDailyChallenge = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="flex flex-1 rounded-lg bg-secondary-light p-4 md:p-5 items-center my-4">
+    <div
+      onClick={() => handleOpenDailyChallenge()}
+      className="flex flex-1 rounded-lg bg-secondary-light p-4 md:p-5 items-center my-4 cursor-pointer"
+    >
       <div className="flex gap-6 flex-col">
         <h4 className="text-xl md:text-4xl">Desafio do dia</h4>
         <p>Complete o desafio, ganhe pontos e avance no ranking!</p>
@@ -14,6 +24,17 @@ const DailyChallengeBanner = () => {
         alt="Tutorial image"
         className="ml-auto w-[160px] h-[160px] md:w-[240px] md:h-[240px]"
       />
+      <Dialog open={isOpen}>
+        <DialogContent className="pointer-events-none">
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your account and remove your data from our
+              servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
