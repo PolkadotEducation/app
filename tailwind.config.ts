e6ae00986b73c3909253a12857b20c43988c7bc3
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2024-12-18 17:18:49 -0300

    create rank home screen banner

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,7 @@ const config = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        "secondary-main": "hsl(var(--secondary-main))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
