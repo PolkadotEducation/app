0ecc087af02f754e82f786c1c13f61d83307c3c0
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2025-01-22 16:01:17 -0300

    add integration and mobile interactions

--- app/(app)/lesson/[courseId]/[id]/page.tsx
+++ app/(app)/lesson/[courseId]/[id]/page.tsx
@@ -5,12 +5,10 @@ import LessonRenderer from "@/components/ui/renderer";
 import Loading from "@/components/ui/loading";
 import { useCourse } from "@/hooks/useCourse";
 import { useUser } from "@/hooks/useUser";
-import { CourseProgress } from "@/types/progressTypes";
-import { getCourseProgress } from "@/api/progressService";
+import { CourseProgress, CourseSummary } from "@/types/progressTypes";
+import { getCourseProgress, getCourseSummary } from "@/api/progressService";
 import CourseProgressTracker from "@/components/ui/courseProgressTracker";
 import CourseDescriptionSection from "@/components/ui/courseDescriptionSection";
-import { getCourse } from "@/api/courseService";
-import { CourseType } from "@/types/courseTypes";
 
 interface Params {
   courseId: string;
@@ -25,13 +23,12 @@ const LessonPage = ({ params }: { params: Params }) => {
 
   const { userLoading, user } = useUser();
   const [courseProgress, setCourseProgress] = useState<CourseProgress>();
-  const [course, setCourse] = useState<CourseType>();
-
+  const [course, setCourse] = useState<CourseSummary>();
   const fetchCourseProgress = async () => {
     const progress = await getCourseProgress({ courseId });
     setCourseProgress(progress);
-    const courseData = await getCourse(courseId);
-    setCourse(courseData);
+    const response = await getCourseSummary({ courseId });
+    setCourse(response);
   };
 
   useEffect(() => {
@@ -56,8 +53,8 @@ const LessonPage = ({ params }: { params: Params }) => {
   if (!selectedLesson) return <div>Lesson not found</div>;
 
   return (
-    <div className="pt-4 flex gap-11">
-      <CourseDescriptionSection classname="sticky top-0" courseModules={course} courseProgress={courseProgress} />
+    <div className="pt-4 flex md:gap-11 flex-col md:flex-row">
+      <CourseDescriptionSection classname="sticky top-0" courseModules={course} />
       <div className="pt-4">
         {courseProgress && <CourseProgressTracker {...courseProgress} />}
         <LessonRenderer
--- components/ui/courseDescriptionSection.tsx
+++ components/ui/courseDescriptionSection.tsx
@@ -1,33 +1,32 @@
-import React, { useState } from "react";
+"use client";
+import React from "react";
 import { Drawer, DrawerClose, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import Image from "next/image";
 import xMark from "../../public/assets/icons/CloseFilled.svg";
 import CourseChapterAccordion from "./courseChapterAccordion";
 import CourseLesson from "./courseLesson";
-import { CourseType } from "@/types/courseTypes";
-import { CourseProgress } from "@/types/progressTypes";
+import { CourseSummary } from "@/types/progressTypes";
+import chevronRight from "../../public/assets/icons/chevronRight.svg";
+import useIsMobile from "@/hooks/useIsMobile";
 
 interface CourseDescriptionSectionProps {
-  courseModules?: CourseType;
+  courseModules?: CourseSummary;
   classname?: string;
-  courseProgress?: CourseProgress;
 }
-const CourseDescriptionSection = ({ courseModules, classname, courseProgress }: CourseDescriptionSectionProps) => {
-  const [isMobile] = useState(false);
-  const [openItem, setOpenItem] = useState<string | null>(null);
 
-  const handleAccordionChange = (value: string | null) => {
-    setOpenItem((prev) => (prev === value ? null : value));
-  };
+const CourseDescriptionSection = ({ courseModules, classname }: CourseDescriptionSectionProps) => {
+  const isMobile = useIsMobile();
 
-  // console.log(courseProgress?.modulesProgress["678fe9755920e561f197f692"]);
-  console.log(courseModules);
   return (
-    <>
+    <div>
+      <h6>Resumo do curso</h6>
       {isMobile ? (
         <Drawer direction="left">
-          <DrawerTrigger>Open</DrawerTrigger>
+          <DrawerTrigger className="h-[max-content] text-body2 text-primary flex items-center gap-4">
+            Exibir progresso
+            <Image src={chevronRight} alt="chevron down" width={8} className="text-primary" />
+          </DrawerTrigger>
           <DrawerContent className="h-screen w-[300px] rounded-tr-[8px] rounded-br-[8px] rounded-tl-none">
             <DrawerClose className="w-[max-content] absolute right-4 top-4">
               <Image src={xMark} alt="close button" />
@@ -35,14 +34,14 @@ const CourseDescriptionSection = ({ courseModules, classname, courseProgress }: 
             <DrawerHeader className="flex items-center justify-center pt-14">
               <DrawerTitle>Resumo do curso</DrawerTitle>
             </DrawerHeader>
-            <Accordion type="single" onValueChange={handleAccordionChange}>
-              {courseModules?.modules?.map((module, index) => {
+            <Accordion type="single" collapsible>
+              {courseModules?.modules.map((module, index) => {
                 return (
-                  <AccordionItem value={`item${index}`} className="px-6" key={module._id}>
+                  <AccordionItem value={`item${index}`} className="px-6" key={module.title}>
                     <AccordionTrigger className="text-sm flex flex-row-reverse justify-between w-full pr-8">
                       <CourseChapterAccordion
                         chapterName={`Module ${index + 1}`}
-                        completed={module.completed}
+                        completed={module.isCompleted}
                         moduleTitle={module?.title}
                       />
                     </AccordionTrigger>
@@ -51,10 +50,10 @@ const CourseDescriptionSection = ({ courseModules, classname, courseProgress }: 
                         {module.lessons.map((lesson, index) => {
                           return (
                             <CourseLesson
-                              key={lesson._id}
+                              key={index}
                               lessonName={lesson.title}
-                              expAmount={lesson.expAmount}
-                              completed={courseProgress?.modulesProgress[index]}
+                              expAmount={lesson.expEarned}
+                              completed={lesson.expEarned > 0}
                             />
                           );
                         })}
@@ -70,29 +69,27 @@ const CourseDescriptionSection = ({ courseModules, classname, courseProgress }: 
         <div
           className={`h-screen w-[400px] rounded-tr-[8px] rounded-br-[8px] rounded-tl-none flex flex-col items-start ${classname}`}
         >
-          <div className="flex items-center justify-center pt-14">
-            <h6>Resumo do curso</h6>
-          </div>
-          <Accordion type="single" className="w-full">
-            {courseModules?.modules?.map((module, index) => {
+          <div className="flex items-center justify-center pt-14"></div>
+          <Accordion type="single" collapsible className="w-full">
+            {courseModules?.modules.map((module, index) => {
               return (
                 <AccordionItem value={`item${index}`}>
                   <AccordionTrigger className="text-sm flex flex-row-reverse justify-between w-full pr-8">
                     <CourseChapterAccordion
                       chapterName={`Module ${index + 1}`}
-                      completed={module.completed}
+                      completed={module.isCompleted}
                       moduleTitle={module?.title}
                     />
                   </AccordionTrigger>
                   <AccordionContent className="mx-12 pl-4 border-l-2 border-neutral-300 rounded-sm">
                     <ul className="pt-4 flex flex-col gap-4">
-                      {module.lessons.map((lesson) => {
+                      {module.lessons.map((lesson: { title: string; expEarned: number }) => {
                         return (
                           <CourseLesson
-                            key={lesson._id}
+                            key={index}
                             lessonName={lesson.title}
-                            expAmount={lesson.expAmount}
-                            completed={lesson.completed}
+                            expAmount={lesson.expEarned}
+                            completed={lesson.expEarned > 0}
                           />
                         );
                       })}
@@ -104,7 +101,7 @@ const CourseDescriptionSection = ({ courseModules, classname, courseProgress }: 
           </Accordion>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
