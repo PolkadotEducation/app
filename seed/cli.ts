99375391b9cf103f40309b205801feeed8772424
Lauro Gripa <laurogripa@gmail.com>
2025-08-08 13:44:31 -0300

    Adapt MDX editor to ResponsiveIframe and dark mode (#489)

--- seed/cli.ts
+++ seed/cli.ts
@@ -7,6 +7,7 @@ import {
   seedAll,
   seedCoursesOnly,
   seedCorrectChoices,
+  partiallyResetDatabase,
 } from "./db";
 
 async function executeWithDb(command: string, callback: (_db: Db) => Promise<void>) {
@@ -40,6 +41,9 @@ async function main() {
         break;
       case "reset":
         await resetDatabase();
+        break;
+      case "reset:partial":
+        await partiallyResetDatabase();
         break;
       default:
         console.error("Invalid command.");
